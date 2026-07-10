78a3c60f81c2e9a9bc07eb0e09eaa9936799cb3b
Daan <daan@example.com>
2026-07-10 13:44:12 +0200

    Fix widget update interval to 15 minutes with exact alarms

--- android/app/src/main/kotlin/com/danield/weerapp/WeatherWidgetProvider.kt
+++ android/app/src/main/kotlin/com/danield/weerapp/WeatherWidgetProvider.kt
@@ -7,6 +7,7 @@ import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import android.widget.RemoteViews
 
